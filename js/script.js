69eb111176e6647ce317b613d0a35ba01d26bb0c
Exist Model <sarizava@yandex.ru>
2017-12-22 14:33:42 +0300

    Add dekstop

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    scrollingSpeed: 1000,
+    responsiveWidth: 1023
+  });
+});
