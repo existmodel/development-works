523c548263f5137bb33594404580aee221d9be05
Exist Model <sarizava@yandex.ru>
2017-12-27 16:35:31 +0300

    Add button-up scroll

--- js/script.js
+++ js/script.js
@@ -10,35 +10,41 @@ $(document).ready(function() {
       var navToggle = document.querySelector(".main-nav__toggle");
       var navMainList = navMain.querySelector(".main-nav__list");
 
-      navMainList.classList.remove("main-nav--nojs");
-
       navToggle.addEventListener("click", function() {
-        if (navMainList.classList.contains("main-nav--closed")) {
-          navMainList.classList.remove("main-nav--closed");
-          navMainList.classList.add("main-nav--opened");
+        if (navMainList.classList.contains("main-nav__list--closed")) {
+          navMainList.classList.remove("main-nav__list--closed");
+          // navMainList.classList.add("main-nav--opened");
         } else {
-          navMainList.classList.add("main-nav--closed");
-          navMainList.classList.remove("main-nav--opened");
+          navMainList.classList.add("main-nav__list--closed");
+          // navMainList.classList.remove("main-nav--opened");
         }
       });
 
 
 
-$(document).ready(function(){   
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('#scroller').fadeIn();
-        } else {
-            $('#scroller').fadeOut();
-        }
-    });
-    $('#scroller').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
-});
+// $(document).ready(function(){   
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 0) {
+//             $('#scroller').fadeIn();
+//         } else {
+//             $('#scroller').fadeOut();
+//         }
+//     });
+//     $('#scroller').click(function () {
+//         $('body,html').animate({
+//             scrollTop: 0
+//         }, 400);
+//         return false;
+//     });
+// });
 
-$(document).ready(function(){
+// $(document).ready(function(){
+// });
+
+$(function(){
+  //scroll to top
+  $('.button-up').click(function(){
+    $('html, body').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
 });
