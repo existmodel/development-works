50121abd44172a076cf6f916d1af0c95133e2550
Exist Model <sarizava@yandex.ru>
2017-12-27 16:06:27 +0300

    Add mobile, tablet v

--- js/script.js
+++ js/script.js
@@ -5,3 +5,40 @@ $(document).ready(function() {
     responsiveWidth: 1023
   });
 });
+
+      var navMain = document.querySelector(".main-nav");
+      var navToggle = document.querySelector(".main-nav__toggle");
+      var navMainList = navMain.querySelector(".main-nav__list");
+
+      navMainList.classList.remove("main-nav--nojs");
+
+      navToggle.addEventListener("click", function() {
+        if (navMainList.classList.contains("main-nav--closed")) {
+          navMainList.classList.remove("main-nav--closed");
+          navMainList.classList.add("main-nav--opened");
+        } else {
+          navMainList.classList.add("main-nav--closed");
+          navMainList.classList.remove("main-nav--opened");
+        }
+      });
+
+
+
+$(document).ready(function(){   
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('#scroller').fadeIn();
+        } else {
+            $('#scroller').fadeOut();
+        }
+    });
+    $('#scroller').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
+
+$(document).ready(function(){
+});
